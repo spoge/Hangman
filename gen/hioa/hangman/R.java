42456426013047199b90e081f88cb7901cc252fc
Joakim Rishaug <Joakimrishaug@gmail.com>
2014-09-04 06:07:41 +0200

    drawable-name changed, norwegian language start

--- gen/hioa/hangman/R.java
+++ gen/hioa/hangman/R.java
@@ -72,20 +72,21 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int buttondrawable=0x7f020001;
-        public static final int hangman_0=0x7f020002;
-        public static final int hangman_1=0x7f020003;
-        public static final int hangman_10=0x7f020004;
-        public static final int hangman_2=0x7f020005;
-        public static final int hangman_3=0x7f020006;
-        public static final int hangman_4=0x7f020007;
-        public static final int hangman_5=0x7f020008;
-        public static final int hangman_6=0x7f020009;
-        public static final int hangman_7=0x7f02000a;
-        public static final int hangman_8=0x7f02000b;
-        public static final int hangman_9=0x7f02000c;
-        public static final int ic_launcher=0x7f02000d;
-        public static final int underline9patch=0x7f02000e;
+        public static final int backgroundmenu=0x7f020001;
+        public static final int buttondrawable=0x7f020002;
+        public static final int hangman_0=0x7f020003;
+        public static final int hangman_1=0x7f020004;
+        public static final int hangman_10=0x7f020005;
+        public static final int hangman_2=0x7f020006;
+        public static final int hangman_3=0x7f020007;
+        public static final int hangman_4=0x7f020008;
+        public static final int hangman_5=0x7f020009;
+        public static final int hangman_6=0x7f02000a;
+        public static final int hangman_7=0x7f02000b;
+        public static final int hangman_8=0x7f02000c;
+        public static final int hangman_9=0x7f02000d;
+        public static final int ic_launcher=0x7f02000e;
+        public static final int underline9patch=0x7f02000f;
     }
     public static final class id {
         public static final int action_settings=0x7f0a000e;
